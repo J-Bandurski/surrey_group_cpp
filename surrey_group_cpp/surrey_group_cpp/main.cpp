94048b985ac7492d8d2f37f71d9d74b6b97cfa39
Jacek <jb01371@surrey.ac.uk>
2017-10-26 14:28:43 +0100

    Initial commit

---
+++ surrey_group_cpp/surrey_group_cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>         // # specifies preprocessor command, these are completed first and foremost 
+//using namespace std;      // using namespace standard, using is matter of personal preference
+
+int main()                  // first executed function
+{
+
+
+
+    return 0;
+}
